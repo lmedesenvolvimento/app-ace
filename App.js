d66c1b27a7e7ef6646b723b161ad96e12c63028b
damisgarcia <damisgarcia@gmail.com>
2017-12-19 19:46:52 -0300

    Remove Lorem

--- App.js
+++ App.js
@@ -24,22 +24,16 @@ export default class App extends React.Component {
     this._cacheResourcesAsync();
   }
 
-  // render() {
-  //   if(this.state.isReady){
-  //     return (
-  //       <StyleProvider style={getTheme(Theme)}>
-  //         <Navigator authorized={this.state.isAuthorized}></Navigator>
-  //       </StyleProvider>
-  //     );
-  //   } else{
-  //     return <Expo.AppLoading/>;
-  //   }
-  // }
-
   render() {
-    return(
-      <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-    );
+    if(this.state.isReady){
+      return (
+        <StyleProvider style={getTheme(Theme)}>
+          <Navigator authorized={this.state.isAuthorized}></Navigator>
+        </StyleProvider>
+      );
+    } else{
+      return <Expo.AppLoading/>;
+    }
   }
 
   async _cacheResourcesAsync(){
