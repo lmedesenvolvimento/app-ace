7081a949a41ef211f7b3d21ff0f3c0c9f075a8e3
damisgarcia <damisgarcia@gmail.com>
2018-10-19 11:36:16 -0300

    Repair bug Modal with Alert in IOS

--- services/Toast.js
+++ services/Toast.js
@@ -1,8 +1,9 @@
-import { Platform, ToastAndroid } from "react-native";
+import TimerMixin from 'react-timer-mixin';
+import { Platform, ToastAndroid } from 'react-native';
 
 export function simpleToast(text){
   if (Platform.OS == 'ios'){
-    alert(text);
+    TimerMixin.setTimeout(() => alert(text), 800);
   } else{
     ToastAndroid.show(text, ToastAndroid.SHORT);
   }
