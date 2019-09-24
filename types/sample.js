09a9b12a17fac62bbb8604cec00668617f470bda
Damis Garcia <damisgarcia@gmail.com>
2019-09-24 11:25:25 -0300

    add new types for sample deposits

--- types/sample.js
+++ types/sample.js
@@ -11,6 +11,7 @@ export const SampleType = {
 export const SampleDeposits = {
   a1: [
     { key: 'a1_water_box', label: 'Caixa de Água', value: 1 }
+    { key: 'a1_private_supply', label: 'Abastecimento particular(poço, cacimba)', value: 2 }
   ],
   a2: [
     { key: 'a2_tank', label: 'Tanque', value: 10 },
@@ -23,26 +24,35 @@ export const SampleDeposits = {
     { key: 'a2_well', label: 'Poço', value: 17 },
     { key: 'a2_cistern', label: 'Cisterna', value: 18 },
     { key: 'a2_waterhole', label: 'Cacimba', value: 19 },
-    { key: 'a2_pool', label: 'Piscina', value: 20 },
-    { key: 'a2_plant_pot', label: 'Vaso de Planta', value: 22 }
   ],
   b: [
-    { key: 'b_mobile_deposits', label: 'Depositos Móveis', value: 30 }
+    { key: 'b_plant_pot', label: 'Vaso de planta', value: 31 },
+    { key: 'b_flask', label: 'Frasco', value: 32 },
+    { key: 'b_plate', label: 'Prato', value: 33 },
+    { key: 'b_refri_container', label: 'Recipiente degelo de refrigerador', value: 34 },
+    { key: 'b_drinking_fountain', label: 'Bebedouro', value: 35 },    
   ],
   c: [
-    { key: 'c_drain', label: 'Ralo', value: 40 }
+    { key: 'c_drain', label: 'Ralo', value: 40 },
+    { key: 'c_chute', label: 'Calha', value: 41 },
+    { key: 'c_sanitary', label: 'Sanitário', value: 42 },
+    { key: 'c_pool', label: 'Piscina', value: 42 },
   ],
   d1: [
-    { key: 'd1_tire', label: 'Pneu', value: 50 }
+    { key: 'd1_tire', label: 'Pneu', value: 50 },
+    { key: 'd1_other_undercarriage', label: 'Outro material rodante', value: 51 },
   ],
   d2: [
-    { key: 'd2_construction_material', label: 'Materiais de Construção', value: 60 },
+    { key: 'd2_rubble', label: 'Entulho', value: 60 },
     { key: 'd2_car_part', label: 'Peça de Carro', value: 61 },
     { key: 'd2_bottle', label: 'Garrafa', value: 62 },
     { key: 'd2_can', label: 'Lata', value: 63 },
     { key: 'd2_deposit_plastic', label: 'Deposito Plástico', value: 64 }
   ],
   e: [
-    { key: 'e_recipient_natural', label: 'Recipientes Naturais', value: 70 }
+    { key: 'e_bromeliad_leaves', label: 'Folha de bromélias', value: 70 },
+    { key: 'e_hollow_trees', label: 'Ocos em árvores', value: 71 },
+    { key: 'e_holes_rocks', label: 'Buracos em rocha', value: 72 },
+    { key: 'e_animal_remains', label: 'Resto de animais', value: 73 },
   ]
 }
